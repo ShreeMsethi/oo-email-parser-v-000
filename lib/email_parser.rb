a6e0954d82d38055fc3d45effb2e2269ce5e20f0
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-02-26 06:07:49 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,14 +5,14 @@
 
 class EmailAddressParser
   
-  attr_accessor :email
+  attr_accessor :email_addresses
   
   def initialize(email_addresses)
     @email_addresses = email_addresses
   end
   
   def parse
-    
+    email_addresses.split(",")
   end
   
 end
